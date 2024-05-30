b3460580e138c873c1519b9ce145873a9cb271b8
AhmadSPriaji <ahmadsiddiqp@gmail.com>
2024-05-30 11:29:07 +0700

    change middleware auth using Authorization

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,11 +1,18 @@
+
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
 module.exports = function(req, res, next) {
-  const token = req.header('x-auth-token');
+  const authHeader = req.header('Authorization');
+
+  if (!authHeader) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  const token = authHeader.split(' ')[1]; // Ambil token setelah 'Bearer'
 
   if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
+    return res.status(401).json({ msg: 'Authorization token not found' });
   }
 
   try {
