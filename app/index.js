066164d08e77988681a69a78ab289427ec412be5
AhmadSPriaji <ahmadsiddiqp@gmail.com>
2024-06-01 20:30:28 +0700

    add cors

--- app/index.js
+++ app/index.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const config = require('./config/config');
 const scoreRoutes = require('./routes/score');
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
